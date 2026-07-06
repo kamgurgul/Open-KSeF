8e88233ac9520c08f29188655f99023e0d77bba3
Kamil Gurgul <kamgurgul@gmail.com>
2026-07-06 08:08:46 +0200

    Update android signing config

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.io.FileInputStream
+import java.util.Properties
+import kotlin.apply
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeCompiler)
@@ -19,9 +23,38 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    signingConfigs {
+        getByName("debug") {
+            val debugSigningConfig = AndroidSigningConfig.getDebugProperties(rootProject.rootDir)
+            storeFile = file(debugSigningConfig.getProperty(AndroidSigningConfig.KEY_PATH))
+            keyAlias = debugSigningConfig.getProperty(AndroidSigningConfig.KEY_ALIAS)
+            keyPassword = debugSigningConfig.getProperty(AndroidSigningConfig.KEY_PASS)
+            storePassword = debugSigningConfig.getProperty(AndroidSigningConfig.KEY_PASS)
+        }
+        create("release") {
+            val releaseSigningConfig =
+                AndroidSigningConfig.getReleaseProperties(rootProject.rootDir)
+            storeFile = file(releaseSigningConfig.getProperty(AndroidSigningConfig.KEY_PATH))
+            keyAlias = releaseSigningConfig.getProperty(AndroidSigningConfig.KEY_ALIAS)
+            keyPassword = releaseSigningConfig.getProperty(AndroidSigningConfig.KEY_PASS)
+            storePassword = releaseSigningConfig.getProperty(AndroidSigningConfig.KEY_PASS)
+        }
+    }
     buildTypes {
-        getByName("release") {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
+            enableUnitTestCoverage = true
+            applicationIdSuffix = ".debug"
         }
     }
     compileOptions {
@@ -36,4 +69,49 @@ dependencies {
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
+}
+
+object AndroidSigningConfig {
+
+    const val KEY_PATH = "KEYSTORE_PATH"
+    const val KEY_PASS = "KEYSTORE_PASS"
+    const val KEY_ALIAS = "KEYSTORE_ALIAS"
+
+    fun getDebugProperties(rootDir: File) =
+        Properties().apply {
+            setProperty(KEY_PATH, "${rootDir.path}/androidApp/debug.keystore")
+            setProperty(KEY_PASS, "android")
+            setProperty(KEY_ALIAS, "androiddebugkey")
+            setProperty(KEY_PASS, "android")
+        }
+
+    fun getReleaseProperties(rootDir: File): Properties {
+        val releaseProperties = Properties()
+        try {
+            releaseProperties.load(FileInputStream(File(rootDir, "local.properties")))
+        } catch (_: Exception) {
+            println("Cannot load local.properties")
+        }
+        return if (
+            releaseProperties.getProperty(KEY_PATH, "").isNotEmpty() &&
+            releaseProperties.getProperty(KEY_PASS, "").isNotEmpty() &&
+            releaseProperties.getProperty(KEY_ALIAS, "").isNotEmpty()
+        ) {
+            println("Using local.properties for signing")
+            releaseProperties
+        } else if (
+            !System.getenv(KEY_PATH).isNullOrEmpty() &&
+            !System.getenv(KEY_PASS).isNullOrEmpty() &&
+            !System.getenv(KEY_ALIAS).isNullOrEmpty()
+        ) {
+            println("Using system env variables for signing")
+            releaseProperties[KEY_PATH] = System.getenv(KEY_PATH)
+            releaseProperties[KEY_PASS] = System.getenv(KEY_PASS)
+            releaseProperties[KEY_ALIAS] = System.getenv(KEY_ALIAS)
+            releaseProperties
+        } else {
+            println("!!!Warning: release keystore not found -> using debug!!!")
+            getDebugProperties(rootDir)
+        }
+    }
 }
